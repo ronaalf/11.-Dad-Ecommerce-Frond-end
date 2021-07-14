4fd048d1f45e4f4020c37c5f18ca9559c0996200
ronaalf <alfaro.ronald2@gmail.com>
2021-07-13 21:54:21 -0600

    add project

---
+++ js/01-file.js
@@ -0,0 +1,24 @@
+'use strict'
+/*var mybutton = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        mybutton.style.display = "block";  
+    }else{
+        mybutton.style.display = "none";
+    }
+}
+function topfunction() {
+    window.scrollTo(0,0);            
+}
+*/
+
+// function narbar(a,b){
+//     var hide = a.hide();
+//     var show = b.show();
+// };
+ 
+var section = document.getElementsByTagName("section");
+
